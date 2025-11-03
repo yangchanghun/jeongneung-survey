b834a66393e97319b7482e224e378a4f63719d1d
yangchanghun <gory4848@naver.com>
2025-11-03 13:31:46 +0900

    D

--- be/survey/views.py
+++ be/survey/views.py
@@ -137,6 +137,99 @@ def survey_statistics(request):
     })
 
 
+# @api_view(['GET'])
+# def survey_export_csv(request):
+#     """
+#     CSV 형식으로 데이터 내보내기
+#     """
+#     response = HttpResponse(content_type='text/csv; charset=utf-8')
+#     response['Content-Disposition'] = 'attachment; filename="timebank_survey.csv"'
+    
+#     # UTF-8 BOM 추가 (엑셀에서 한글 깨짐 방지)
+#     response.write('\ufeff')
+    
+#     writer = csv.writer(response)
+    
+#     # 헤더 작성
+#     writer.writerow([
+#         'ID',
+#         '1번 시간은행 인지',
+#         '2번 받고 싶은 도움',
+#         # '2번 받고 싶은 도움(기타)',
+#         '3번 줄 수 있는 도움',
+#         # '3번 줄 수 있는 도움(기타)',
+#         '4번 참여 시간',
+#         # '4번 참여 시간(기타)',
+#         '5번 급할 때 연락 가능한 사람',
+#         '6번 참여 형태',
+#         '7번 기대 사항',
+#         # '7번 기대 사항(기타)',
+#         '8번 연령대',
+#         '9번 성별',
+#         '10번 거주지',
+#         '작성일',
+#         '작성시간',
+#     ])
+    
+    
+        
+
+
+#     # 데이터 작성
+#     for survey in TimeBankSurvey.objects.all():
+#         받고싶은도움 = ""
+#         if survey.get_help_receive_display() == "기타":
+#             받고싶은도움 = "기타"+"("+survey.help_receive_other+")"
+#         else:
+#             받고싶은도움 = survey.get_help_receive_display()
+
+#         줄수있는도움= ""
+#         if survey.get_help_give_display() == "기타":
+#             줄수있는도움 = "기타"+"("+survey.help_give_other+")"
+#         else:
+#             줄수있는도움 = survey.get_help_give_display()
+
+
+#         참여시간 = ""
+#         if survey.get_time_commitment_display() == "기타":
+#             참여시간 = "기타"+"("+survey.time_commitment_other+")"
+#         else:
+#             참여시간 = survey.get_time_commitment_display()
+
+#         기대사항 = ""
+#         if survey.get_expectation_display() == "기타":
+#             기대사항 = "기타"+"("+survey.expectation_other+")"
+#         else:
+#             기대사항 = survey.get_expectation_display()
+
+
+#         writer.writerow([
+#             survey.id, # 번호
+#             survey.get_knows_timebank_display(), #시간
+#             # survey.get_help_receive_display(), 
+#             받고싶은도움, #받고싶은 도움
+#             # survey.help_receive_other or '',
+#             # survey.get_help_give_display(),
+#             줄수있는도움, #줄 수 있는 도움
+#             # survey.help_give_other or '',
+#             # survey.get_time_commitment_display(),
+#             참여시간, # 참여시간
+#             # survey.time_commitment_other or '',
+#             survey.get_support_network_display(), #급할때 연락가능한사람
+#             survey.get_participation_type_display(), #참여 형태
+#             # survey.get_expectation_display(),
+#             기대사항, #기대사항
+#             # survey.expectation_other or '',
+#             survey.get_age_group_display(), # 연령대
+#             survey.get_gender_display(), # 성별
+#             survey.get_residence_display(), # 거주지
+#             survey.created_at.strftime('%m-%d'),    # 2025-11-03
+#             survey.created_at.strftime('%H:%M:%S')     # 00:42:30
+#         ])
+    
+#     return response
+from django.utils import timezone  # 상단에 추가!
+
 @api_view(['GET'])
 def survey_export_csv(request):
     """
@@ -155,15 +248,11 @@ def survey_export_csv(request):
         'ID',
         '1번 시간은행 인지',
         '2번 받고 싶은 도움',
-        # '2번 받고 싶은 도움(기타)',
         '3번 줄 수 있는 도움',
-        # '3번 줄 수 있는 도움(기타)',
         '4번 참여 시간',
-        # '4번 참여 시간(기타)',
         '5번 급할 때 연락 가능한 사람',
         '6번 참여 형태',
         '7번 기대 사항',
-        # '7번 기대 사항(기타)',
         '8번 연령대',
         '9번 성별',
         '10번 거주지',
@@ -171,60 +260,65 @@ def survey_export_csv(request):
         '작성시간',
     ])
     
-    
-        
-
-
     # 데이터 작성
     for survey in TimeBankSurvey.objects.all():
+        # 한국 시간으로 변환 ✅
+        local_time = timezone.localtime(survey.created_at)
+        
+        # 받고 싶은 도움
         받고싶은도움 = ""
         if survey.get_help_receive_display() == "기타":
-            받고싶은도움 = "기타"+"("+survey.help_receive_other+")"
+            if survey.help_receive_other:  # ✅ None 체크
+                받고싶은도움 = f"기타({survey.help_receive_other})"
+            else:
+                받고싶은도움 = "기타"
         else:
             받고싶은도움 = survey.get_help_receive_display()
 
-        줄수있는도움= ""
+        # 줄 수 있는 도움
+        줄수있는도움 = ""
         if survey.get_help_give_display() == "기타":
-            줄수있는도움 = "기타"+"("+survey.help_give_other+")"
+            if survey.help_give_other:  # ✅ None 체크
+                줄수있는도움 = f"기타({survey.help_give_other})"
+            else:
+                줄수있는도움 = "기타"
         else:
             줄수있는도움 = survey.get_help_give_display()
 
-
+        # 참여 시간
         참여시간 = ""
         if survey.get_time_commitment_display() == "기타":
-            참여시간 = "기타"+"("+survey.time_commitment_other+")"
+            if survey.time_commitment_other:  # ✅ None 체크
+                참여시간 = f"기타({survey.time_commitment_other})"
+            else:
+                참여시간 = "기타"
         else:
             참여시간 = survey.get_time_commitment_display()
 
+        # 기대 사항
         기대사항 = ""
         if survey.get_expectation_display() == "기타":
-            기대사항 = "기타"+"("+survey.expectation_other+")"
+            if survey.expectation_other:  # ✅ None 체크
+                기대사항 = f"기타({survey.expectation_other})"
+            else:
+                기대사항 = "기타"
         else:
             기대사항 = survey.get_expectation_display()
 
-
         writer.writerow([
-            survey.id, # 번호
-            survey.get_knows_timebank_display(), #시간
-            # survey.get_help_receive_display(), 
-            받고싶은도움, #받고싶은 도움
-            # survey.help_receive_other or '',
-            # survey.get_help_give_display(),
-            줄수있는도움, #줄 수 있는 도움
-            # survey.help_give_other or '',
-            # survey.get_time_commitment_display(),
-            참여시간, # 참여시간
-            # survey.time_commitment_other or '',
-            survey.get_support_network_display(), #급할때 연락가능한사람
-            survey.get_participation_type_display(), #참여 형태
-            # survey.get_expectation_display(),
-            기대사항, #기대사항
-            # survey.expectation_other or '',
-            survey.get_age_group_display(), # 연령대
-            survey.get_gender_display(), # 성별
-            survey.get_residence_display(), # 거주지
-            survey.created_at.strftime('%m-%d'),    # 2025-11-03
-            survey.created_at.strftime('%H:%M:%S')     # 00:42:30
+            survey.id,
+            survey.get_knows_timebank_display(),
+            받고싶은도움,
+            줄수있는도움,
+            참여시간,
+            survey.get_support_network_display(),
+            survey.get_participation_type_display(),
+            기대사항,
+            survey.get_age_group_display(),
+            survey.get_gender_display(),
+            survey.get_residence_display(),
+            local_time.strftime('%m-%d'),      # ✅ 한국 시간으로 월-일
+            local_time.strftime('%H:%M:%S'),   # ✅ 한국 시간으로 시:분:초
         ])
     
     return response
