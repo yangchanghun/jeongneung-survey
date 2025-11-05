770c14d3bc09506821e5d7e5c16b710c9e2d0b7e
yangchanghun <gory4848@naver.com>
2025-11-05 09:38:12 +0900

    D

--- be/config/urls.py
+++ be/config/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('survey.urls')),
+    path('api/admin/', admin.site.urls),
+    path('api/survey/', include('survey.urls')),
 ]
